cc29ce3c033380e6cd7ab3b56fc86f4300ecd4e8
Minty <82933018+kjkiwi@users.noreply.github.com>
2022-06-03 10:24:17 +1200

    Could do up to here without assistance Hit issue when needed to push children of state object to <h1>. Problem  is that the inputEntered function doesn't differentiate between the two inputs.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+//1. import useState
+import React, { useState } from "react";
 
 function App() {
+  //2. create state variable that is a JS object with both name input variables as items
+  const [fullName, setFullName] = useState({
+    nameInput1: "",
+    nameInput2: ""
+  });
+
+  //5. create basic function and check by console.logging value of input's name attribute when input is entered
+  function inputEntered(event) {
+    console.log(event.target.name);
+    setFullName(event.target.name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      {/* 6. Add variables representing input values to heading and test by entering*/}
+      <h1>
+        Hello {fullName.nameInput1} {fullName.nameInput2}{" "}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        {/* 3. Add state object item field names as value attributes to control variables */}
+        {/* 4. Add event to call function that will run when user inputs anything */}
+        <input
+          onChange={inputEntered}
+          name="fName"
+          placeholder="First Name"
+          value={fullName.nameInput1}
+        />
+        <input
+          onChange={inputEntered}
+          name="lName"
+          placeholder="Last Name"
+          value={fullName.nameInput2}
+        />
         <button>Submit</button>
       </form>
     </div>
